08de505978bf8514f6db75d7751d689a93b2f576
shreyanshanand <s.anand@queensu.ca>
2019-07-22 19:46:24 -0400

    added the Google Maps link

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
- fbACCESS: process.env.FACEBOOK_ACCESS   
-}
+ fbACCESS: process.env.FACEBOOK_ACCESS,
+ googleMaps: process.env.GOOGLE_MAPS
+};
--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const {fbACCESS} = require('./config');
+const {googleMaps} = require('./config');
+const request = require('request');
 const fb = require('./fb');
 // const { endpoint, masterKey, port } = require(‘./config’);
 
@@ -13,6 +14,12 @@ http.createServer(app).listen(3000, ()=>{
     console.log('Listening on port 3000')
 });
 
+request('https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal+Studios+Hollywood&key='+googleMaps, function (error, response, body) {
+    console.error('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+});
+
 app.post('/endpoint',(req, res) => {
     recepID = req.body['originalDetectIntentRequest']['payload']['data']['sender']['id'];
     let text = req.body['queryResult']['queryText'];
@@ -21,5 +28,6 @@ app.post('/endpoint',(req, res) => {
     console.log(action, parameters);
     if(action == 'busTo'){
         fb.sendMessage(recepID, "Looking for the bus");
+
     }
 });
